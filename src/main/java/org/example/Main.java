7da91e212f2fdbb192e7de0e8d8665ef81f3cf4c
Rahul022294 <rahulk220294@gmail.com>
2024-12-14 13:21:12 +0530

    Intial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        int a = 2;
+        int b=5;
+        int c=10;
+        int atimesb = a*b;
+        if (atimesb == c) {
+            System.out.println("Both are Equal");}
+        else {
+            System.out.println("Not Equal");
+        }
+    }
+
+}
